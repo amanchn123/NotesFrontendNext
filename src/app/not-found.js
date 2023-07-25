e9a9912a61ab1973320239ea6d390101994e998d
amanchn123 <118670647+amanchn123@users.noreply.github.com>
2023-07-25 14:28:32 +0530

    page not found

---
+++ src/app/not-found.js
@@ -0,0 +1,17 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+ 
+export default function Page() {
+  const router = useRouter()
+  useEffect(()=>{
+    router.push('/')
+  })
+
+  return (
+    <button type="button" onClick={() => router.push('/')}>
+      Dashboard
+    </button>
+  )
+}
